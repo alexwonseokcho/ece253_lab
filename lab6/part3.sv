16d2753832e1732633cdb4757c39f8c05a3ae9db
Garvishb <togarvish@gmail.com>
2023-11-16 10:56:48 -0500

    datapath done, testing required

--- lab6/part3.sv
+++ lab6/part3.sv
@@ -1,37 +1,71 @@
 module datapath(
-    input logic [4:0] a,
-    input logic [3:0] d,
-    input logic [3:0] D,
-    output logic [4:0] new_a,
-    output logic [3:0] new_d
+    input logic clk,
+    input logic reset,
+    input [3:0] DataIn,
+    input logic ld_D, // enable for Dividend
+    input logic ld_d, // enable for Divisor
+    input logic load_res, // mux select for loading the result in Dividend
+    input result_valid,
+
+    output logic [3:0] Quotient,
+    output logic [4:0] Remainder,
     );
-
-    logic [3:0] a_lsb;
-    logic d_msb;
-    assign a_lsb = a[3:0]; // assign outside the always block
-    assign d_msb = D[3];
+    logic [3:0] d, // divisor
+    logic [3:0] D, // dividend
+    logic [4:0] a; // Register A
+    assign a = 5'b00000; // I don't know if this is needed
+    logic [3:0] a_lsb; // least significant 4 bits of A
+    logic D_msb; // most significant bit of Dividend
     logic [4:0] reg_shifted;
-    assign reg_shifted = {a_lsb, d_msb}; 
     logic [4:0] sub_result;
-    assign sub_result = reg_shifted - d;
     logic sub_result_msb;
-    assign sub_result_msb = sub_result[4];
     // logic [4:0] new_a;
-    assign new_a = sub_result_msb ? reg_shifted : sub_result;
-
     logic [3:0] D_lsb;
-    assign D_lsb = D[3:0]; 
-    // logic [4:0] new_d;
-    assign new_d = {D_lsb, ~sub_result_msb};
+    logic [4:0] new_D;
 
-    // always_ff @(posedge clk) begin
-    //     if (reset) begin
-    //         a <= 0;
-    //         D <= 0;
-    //     end
-    //     else begin
-    //         a <= new_a;
-    //         D <= new_d;
-    //     end
-    // end
+    // assigning the values to Dividend and Divisor
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            d <= 4'b0000;
+            D <= 4'b0000;
+        end
+        else begin
+            if (ld_d) d <= DataIn;
+            if (ld_D) D <= load_res ? new_D : DataIn;
+        end
+    end
+
+    // the actual division
+    always@(posedge clk) 
+    begin
+        if (reset)
+        begin
+            a <= 5'b00000;
+        end
+        else
+        begin
+            a_lsb = a[3:0]; // assign used outside the always block
+            D_msb = D[3];
+            reg_shifted = {a_lsb, D_msb}; 
+            sub_result = reg_shifted - d;
+            sub_result_msb = sub_result[4];
+            a = sub_result_msb ? reg_shifted : sub_result;
+            D_lsb = D[3:0]; 
+            new_D = {D_lsb, ~sub_result_msb}; 
+        end
+    end
+
+    // assigning the values to Quotient and Remainder when division is done
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            Quotient <= 4'b0000;
+            Remainder <= 5'b00000;
+        end
+        else begin
+            if (result_valid) begin
+                Quotient <= D;
+                Remainder <= a;
+            end
+        end
+    end
 endmodule
